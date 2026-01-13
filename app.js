f2f71c3ca2fc2e4097e285691043a6446c0ecf81
Rob_Pevny <your_email>
2026-01-12 19:14:27 -0700

    auth

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const cakeRoutes = require("./Routes/cakes");
 // Allows Cross-Origin-Resource sharing
 var whitelist = [
   'http://localhost:4200', 
-  'https://school-client-i3jl.onrender.com'
+  'https://school-client-i3jl.onrender.com',
+  
 ];
 var corsOptions = {
   origin: function (origin, callback) {
